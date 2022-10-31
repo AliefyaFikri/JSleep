a6bc7dfa5871d1fff6a079fbd1a848f141a98ff5
Aliefya Fikri Ihsani <aliefya.fikri@ui.ac.id>
2022-10-31 13:51:02 +0700

    TP Modul 6 - Menambahkan city.json dll

--- src/aliefyaFikriIhsaniJSleepMN/JSleep.java
+++ src/aliefyaFikriIhsaniJSleepMN/JSleep.java
@@ -1,116 +1,35 @@
 package aliefyaFikriIhsaniJSleepMN;
-import java.sql.*;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 public class JSleep {
 
-    public static void main(String[] args){
-        ArrayList<Room> RoomSerialized = new ArrayList<Room>();
-
-        for (int i = 0; i < 5; i++){
-            RoomSerialized.add(i, JSleep.createRoom());
-            System.out.println(RoomSerialized.get(i).toString());
-        }
-    }
-    public static Room createRoom(){
-        Price price = new Price(100000, 0.5);
-        Room room = new Room("Hotel", 30, price, Facility.AC, City.DEPOK, "Jalan Margonda Raya");
-        return room;
-    }
-/*        Payment testPayment = new Payment(2, 2, 2,2);
-        System.out.println(testPayment.getTime());
-        System.out.println(testPayment.getDuration());
-        Price[] unfilteredArray = new Price[5];
-        for(int i=0;i < unfilteredArray.length;i++){
-         int j = 5000;
-         unfilteredArray[i] = new Price((i+1)*j);
-        }
-        System.out.println("Price List");
-        for(int i=0;i < unfilteredArray.length;i++){
-         System.out.println(unfilteredArray[i].price);
-        }
-        System.out.println("Below 12000.0");
-        System.out.println(Validate.filter(unfilteredArray, 12000,true));
-        System.out.println("Above 10000.0");
-        System.out.println(Validate.filter(unfilteredArray, 10000,false));
-*/
-/*       
-        Complaint testComplain = new Complaint(1,"23 August 2022","Bad Quality");
-        Price testPrice = new Price(100000, 20000);
-        Room testRoom = new Room (1,"Presidential Suite", 5 ,testPrice,
-        Facility.FitnessCenter, City.DEPOK, "JL. Margonda Raya");
-        Account testAccount = new Account(1, "Bob", "bob@gmail.com", "bob");
-        Rating testRating = new Rating();
-        System.out.println(testComplain.toString());
-        System.out.println(testRoom.toString());
-        System.out.println(testAccount.toString());
-        System.out.println(testPrice.toString());
-        System.out.println(testRating.toString());
-*/       
-        
-        
-        
-/*        
-        Payment testRoom = new Payment(1, 1, 1, "", 1, "", "");
-        Invoice testInvoice = new Invoice(2,2,2, "");
-        System.out.println(testRoom.print());
-        System.out.println(testInvoice.print()); 
-*/ 
-/*        Room test = createRoom();
-        
-        System.out.println(test.name);
-        System.out.println(test.size);
-        System.out.println(test.price.price);
-        System.out.println(test.facility);        
- */       
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
     }
 
-/*   
-    public static int getHotelId(){
-        return 0;
-    }
-    
-    public static String getHotelName(){
-        return "hotel";
-    }
-    
-    public static boolean isDiscount(){
-        return true;
-    }
-    
-    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount){
-        if(beforeDiscount<afterDiscount){
-            return 0.0f;
-        }
-        else {
-            return (float) ((beforeDiscount-afterDiscount)/10.f) ;
-        }
-    }
-    
-    public static int getDiscountedPrice(int price, float discountPercentage){
-        if (discountPercentage > 100.0f){
-            return 0;
-        } else {
-            return price - (int) (price * (discountPercentage/100.0f));
-        }
-    }
-    
-    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
-        return (int) (discountedPrice / ( 1.0f- (discountPercentage / 100.0f)));
-    }
-    
-    public static float getAdminFeePercentage(){
-        return 0.05f;
+    public static void main (String[] args){
 
+        String filepath= "C:\\Java\\OOP\\JSleep\\city.json" ;
+                Gson gson = new Gson();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: "+ input.name);
+            System.out.println("population: "+input.population);
+            System.out.println("states: ");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
-    
-    public static int getAdminFee(int price){
-        return (int) (getAdminFeePercentage() * price);
 
-    }
-    
-    public static int getTotalPrice(int price, int numberOfNight){
-        return (price * numberOfNight) + getAdminFee(price * numberOfNight);
-    }
-*/
+}
 
---
+++ uts test/Main.java
@@ -0,0 +1,11 @@
+import java.util.Calendar;
+
+public class Main
+{
+    
+    public static void main(String[]args){// instance variables - replace the example below with your own
+      
+    int myArray[] = new int[10];
+    System.out.println(myArray[0]);
+}    
+}
